1beb574dcaa607c2a8255242e74c34c4ac4b13db
Rickeslei Guerra <rickesleiguerra@gmail.com>
2018-04-25 05:54:22 -0300

    Project started

---
+++ src/main/java/com/java/uml/JavaumlApplication.java
@@ -0,0 +1,12 @@
+package com.java.uml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaumlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaumlApplication.class, args);
+	}
+}
