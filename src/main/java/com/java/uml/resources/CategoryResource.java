bd240c10ad36a7a4c09a87788330e26c8ddcacb2
Rickeslei Guerra <rickesleiguerra@gmail.com>
2018-04-25 06:20:58 -0300

    Testing the first class java Domain

--- src/main/java/com/java/uml/resources/CategoryResource.java
+++ src/main/java/com/java/uml/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.java.uml.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.java.uml.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "REST is OK!";
+	public List<Category> list() {
+		
+		Category c1 = new Category(1, "Info");
+		Category c2 = new Category(2, "Office");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(c1);
+		list.add(c2);
+		
+		return list;
 	}
 }
