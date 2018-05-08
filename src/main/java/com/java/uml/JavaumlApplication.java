b9c0256595a1a66f4bc22ee7ff08bcfb54fcf350
Rickeslei Guerra <rickesleiguerra@gmail.com>
2018-05-08 05:40:01 -0300

    Creating instantiation operation

--- src/main/java/com/java/uml/JavaumlApplication.java
+++ src/main/java/com/java/uml/JavaumlApplication.java
@@ -1,12 +1,34 @@
 package com.java.uml;
 
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JavaumlApplication {
+import com.java.uml.domain.Category;
+import com.java.uml.repositories.CategoryRepository;
 
+@SpringBootApplication
+public class JavaumlApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaumlApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Computing");
+		Category cat2 = new Category(null, "Office");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+	}
+	
+	
 }
