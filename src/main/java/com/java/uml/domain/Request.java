8b06d916b3d54ea805b006b3f3aee0616b3a1534
Rickeslei Guerra <rickesleiguerra@gmail.com>
2018-06-04 07:14:21 -0300

    Update JsonIgnore

--- src/main/java/com/java/uml/domain/Request.java
+++ src/main/java/com/java/uml/domain/Request.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Request implements Serializable {
@@ -30,11 +29,9 @@ public class Request implements Serializable {
 	@JsonFormat(pattern="dd/MM/yyyy hh:mm")
 	private Date moment;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="request")
 	private Payment payment;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private Client client;
